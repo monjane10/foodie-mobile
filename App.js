69d8c60f92bc51d2de07feda1bd3039f5fb60c51
monjane10 <louramonja52.n@gmail.com>
2025-04-25 17:50:57 +0200

    Finalizando a segunda tela de registo

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import Login from "./src/screans/login/login.jsx";
 import Registo from "./src/screans/registo/registo.jsx";
+import Registo2 from "./src/screans/registo2/registo2.jsx";
 
 export default function App() {
 
   return (<>
-  <Login/>
+  <Registo2/>
    
   </>
   );
--- src/screans/registo2/registo2.jsx
+++ src/screans/registo2/registo2.jsx
@@ -1,23 +1,23 @@
 import { View, Text, TouchableOpacity, ScrollView } from "react-native";
-import { styles } from "./registo.style.js";
+import { styles } from "./registo2.style.js";
 import Header from "../../components/header/header.jsx";
 import TextBox from "../../components/textbox/textbox.jsx";
 import Button from "../../components/button/button.jsx";
 
 
-function Registo() {
+function Registo2() {
     return(
         
         <ScrollView>
         <View style ={styles.container}>
-        <Header texto="Criar uma conta"/>
+        <Header texto="Dados adicionais"/>
         <View style={styles.formgroup}>
-        <TextBox texto="Nome Completo" />
-        <TextBox texto="Email" />
-        <TextBox texto="Senha" isPassword={true} />
-        <TextBox texto="Confirmar Senha" isPassword={true} />
+        <TextBox texto="Cidade" />
+        <TextBox texto="Bairro" />
+        <TextBox texto="Casa"/>
+        <TextBox texto="Número de BI"/>
         <View style={styles.btn}>
-        <Button texto="Proximo Passo" />
+        <Button texto="Registar" />
         </View>
         </View>
         <View style={styles.footer}>
@@ -31,4 +31,4 @@ function Registo() {
     )
 }
 
-export default Registo;
+export default Registo2;
