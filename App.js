3973d1dee59b148f6f9c07d4eeb99a5bdcc92b99
monjane10 <louramonja52.n@gmail.com>
2025-04-25 13:35:29 +0200

    refactorando o codigo

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { Text } from 'react-native';
 import { styles } from "./style.js";
-import Button from "./src/components/button.jsx";
+import Button from "./src/components/button/button.jsx";
 
 export default function App() {
 
