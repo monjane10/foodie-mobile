a97e979e82a4764a3cccf3f4335966fbf7f73c79
monjane10 <louramonja52.n@gmail.com>
2025-04-25 16:45:17 +0200

    terminando a tela de cadastro

--- src/screans/registo/registo.jsx
+++ src/screans/registo/registo.jsx
@@ -1,29 +1,33 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
 import { styles } from "./registo.style.js";
 import Header from "../../components/header/header.jsx";
 import TextBox from "../../components/textbox/textbox.jsx";
 import Button from "../../components/button/button.jsx";
 
 
-function Login() {
+function Registo() {
     return(
+        <ScrollView>
         <View style ={styles.container}>
-        <Header texto="Entre na tua conta"/>
+        <Header texto="Criar uma conta"/>
         <View style={styles.formgroup}>
+        <TextBox texto="Nome Completo" />
         <TextBox texto="Email" />
         <TextBox texto="Senha" isPassword={true} />
+        <TextBox texto="Confirmar Senha" isPassword={true} />
         <View style={styles.btn}>
-        <Button texto="Entrar" />
+        <Button texto="Proximo Passo" />
         </View>
         </View>
         <View style={styles.footer}>
             <TouchableOpacity>
-            <Text style={styles.footertext}>Criar Conta</Text>
+            <Text style={styles.footertext}>Entrar na minha conta</Text>
             </TouchableOpacity>
            
         </View>
         </View>
+        </ScrollView>
     )
 }
 
-export default Login;
+export default Registo;
---
+++ src/screans/registo/registo.style.js
@@ -0,0 +1,42 @@
+import { COLORS, FONT_SIZE } from "../../constants/theme"
+
+export const styles = {
+    container: {
+       flex: 1,
+       padding: 40,
+       alignItems: "center",
+       justifyContent: "center",
+        
+    },
+
+
+    texto: {
+        fontSize: FONT_SIZE.md,
+        color: "white",
+        padding: 14,
+        textAlign: "center"
+    },
+    formgroup: {
+        width: "100%",
+        marginTop: 50,
+        marginBottom: 40,
+    },
+    btn: {
+        marginTop: 20,
+        width: "100%",
+    },
+    footer: {
+        width: "100%",
+        position: "absolute",
+        bottom: 0,
+        marginBottom: 40,
+
+    },
+
+    footertext: {
+        textAlign: "center",
+        fontSize: FONT_SIZE.md,
+        color: COLORS.dark_gray,
+        padding: 14,
+    }
+}
