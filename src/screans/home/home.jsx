d0f7ca5cf088dfc5b97b68922be6de03a1b1ef2a
monjane10 <louramonja52.n@gmail.com>
2025-04-25 21:33:12 +0200

    Comecando a tela home

--- src/screans/home/home.jsx
+++ src/screans/home/home.jsx
@@ -1,10 +1,25 @@
-
+import {styles} from './home.style.js';
+import { View, Text, Image } from 'react-native'
+import icons from '../../constants/icons.js'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import TextBox from '../../components/textbox/textbox.jsx';
+import { useState } from 'react';
 
 function Home() {
+    const [busca, setBusca] = useState('');
+
     return (
-        <>
-        
-        </>
+        <SafeAreaView style={styles.container}>
+        <View style={styles.headerBar}>
+            <Image source={icons.logo} style={styles.logo} />
+            <Image source={icons.cart} style={styles.cart} />
+        </View>
+        <View style={styles.busca}>
+            <TextBox placeholder="O que vamos pedir hoje?"  onChangeText ={(texto) => setBusca(texto) }
+            value={busca} />
+        </View>
+
+        </SafeAreaView>
     )
 }
 
---
+++ src/screans/home/home.style.js
@@ -0,0 +1,34 @@
+import { COLORS, FONT_SIZE } from "../../constants/theme"
+
+export const styles = {
+    container: {
+       flex: 1,
+       padding: 15,
+       backgroundColor: COLORS.white,
+        
+    },
+
+    headerBar: {
+        height: 45,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+
+    logo: {
+        width: 140,
+        height: 27,
+    },
+
+    cart: {
+        width: 30,
+        height: 30,
+    },
+
+    busca: {
+        marginBottom: 20,
+    }
+
+   
+}
